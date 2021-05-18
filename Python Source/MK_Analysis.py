25b5646091b6cfa4e0323aa054b2792382b4b0b5
cuebits <76832527+cuebits@users.noreply.github.com>
2021-05-18 22:16:20 +1000

    2021-05-18 Update

--- Python Source/MK_Analysis.py
+++ Python Source/MK_Analysis.py
@@ -1,16 +1,19 @@
-from pandas._libs.tslibs import Timestamp
 import pymannkendall
 import numpy
-import sys
-import csv
 import pandas
+import geopandas
+import smoomapy
+import json
 import datetime
+
+from shapely.geometry import Point
+
+mk_significance_alpha = 0.05
 
 def main():
 
     # Read CSV file into a pandas data frame 
     data_frame = pandas.read_csv("US_Data.csv", sep=",")
-    print(data_frame)
 
     # Extract the dates column and place it in a series
     dates = pandas.Series(data_frame["Date"])
@@ -22,14 +25,17 @@ def main():
     # Convert to a time series
     dates = pandas.to_datetime(dates)
 
-    # Create a new data frame for Sen's Slope values
+    # Create new data frames for Sen's Slopes, M-K Tests, and Coordinates
     sens_dataframe = pandas.DataFrame()
+    mk_dataframe = pandas.DataFrame()
+    coor_dataframe = pandas.DataFrame()
 
     # Iterate for each station in the data frame
     for label, content in data_frame.iteritems():
         
         # If input file has lat/lon rows, drop the two rows
         if True:
+            coor_dataframe[label] = content[0, 1]
             content = content.drop(index = [0, 1])
 
         # Skip the dates column
@@ -43,36 +49,48 @@ def main():
         monthly_array = content.values                              # Create an array with the values from the current data frame column
         monthly_array = numpy.reshape(monthly_array, (-1, 12)).T    # Reshape to 12 x N, then transpose so each array index corresponds with a month
 
-        # Create a temporary column for storing into the new data frame
-        temp = []
+        # Create temporary columns for storing into the new data frame
+        sens_temp, mk_temp = []
 
         # For each column in the array, run the tests
         for month in monthly_array:
             
-            # Append the desired test results to the temporary column
-            result = run_tests(month)
-            temp.append(result.slope)
+            # Append the desired test results to the sens_temporary column
+            sens_result, mk_result = run_tests(month, mk_significance_alpha)
+            sens_temp.append(sens_result.slope)
+            mk_temp.append(mk_result.trend)
 
-        print(label)
-        print(temp)
+        # Add each station to the data frames
+        sens_dataframe[label] = sens_temp
+        mk_dataframe[label] = mk_temp
 
-        # Add each station to the data frame
-        sens_dataframe[label] = temp
-
+    # Set data frame indexes
     sens_dataframe["Month"] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     sens_dataframe = sens_dataframe.set_index("Month")
 
-    print(sens_dataframe)
+    mk_dataframe["Month"] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    mk_dataframe = mk_dataframe.set_index("Month")
 
-    sens_dataframe.to_csv("output.csv")
+    coor_dataframe["Coordinate"] = ["Latitude", "Longitude"]
+    coor_dataframe = coor_dataframe.set_index("Coordinate")
+
+    # Set file names and output files
+
+    file_time = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+
+    sens_dataframe.T.to_csv("sens_" + file_time + ".csv")
+    mk_dataframe.T.to_csv("mk_" + file_time + ".csv")
+    coor_dataframe.T.to_csv("coordinates_" + file_time + ".csv")
+    
 
 
 # Function for the tests to be conducted over each data series
-def run_tests(data):
+def run_tests(data, alpha):
 
     slope_test = pymannkendall.sens_slope(data)
+    mk_test = pymannkendall.original_test(data, alpha=alpha)
 
-    return slope_test
+    return slope_test, mk_test
 
 
 
